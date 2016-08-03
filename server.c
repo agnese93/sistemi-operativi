7490ef09962b08fe65d3bd55e85ba1b605cb865c
Stefano Germani <stefger88@gmail.com>
2016-08-03 12:12:07 +0200

    client legge messaggi dal server

--- server.c
+++ server.c
@@ -8,6 +8,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+char messaggi[] = "Messaggio 1\nMessaggio 2\n";
+
 void error(const char *msg)
 {
     perror(msg);
@@ -23,6 +25,7 @@ int leggiPorta(int argc, char *argv[]){
  		return atoi(argv[1]);
  	}
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -45,18 +48,22 @@ int main(int argc, char *argv[])
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
+			  
+	  while(1 == 1){
+	      listen(sockfd,5);
+	      clilen = sizeof(cli_addr);
+	      newsockfd = accept(sockfd, 
+	                  (struct sockaddr *) &cli_addr, 
+	                  &clilen);
+	      if (newsockfd < 0) 
+	           error("ERROR on accept");
+	      bzero(buffer,256);
+
+
+	      n = write(newsockfd, messaggi, sizeof(messaggi));
+	  }
+
+	 
      if (n < 0) error("ERROR writing to socket");
      close(newsockfd);
      close(sockfd);
